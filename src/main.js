da03c362378414b360488387d1b6b012b9f0be84
RezaBakhshiNia <Reza.bakhshi1381r@gmail.com>
2025-03-07 08:02:00 +0330

    format the document

--- src/main.js
+++ src/main.js
@@ -1,66 +1,59 @@
 // Setup canvas and context
-const canvas = document.getElementById('mainCanvas');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const canvas = document.getElementById('mainCanvas')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
-const W = window.innerWidth;
-const H = window.innerHeight;
+const W = window.innerWidth
+const H = window.innerHeight
 
 // Helper functions
-const PI2 = Math.PI * 2;
-const random = (min, max) => Math.random() * (max - min + 1) + min | 0;
-const timestamp = () => new Date().getTime();
+const PI2 = Math.PI * 2
+const random = (min, max) => (Math.random() * (max - min + 1) + min) | 0
+const timestamp = () => new Date().getTime()
 
 // Fireworks Setup
 class Birthday {
   constructor() {
-    this.resize();
-    this.fireworks = [];
-    this.counter = 0;
-    this.then = timestamp();
+    this.resize()
+    this.fireworks = []
+    this.counter = 0
+    this.then = timestamp()
   }
 
   resize() {
-    this.width = canvas.width = window.innerWidth;
-    let center = this.width / 2 | 0;
-    this.spawnA = center - center / 4 | 0;
-    this.spawnB = center + center / 4 | 0;
+    this.width = canvas.width = window.innerWidth
+    let center = (this.width / 2) | 0
+    this.spawnA = (center - center / 4) | 0
+    this.spawnB = (center + center / 4) | 0
 
-    this.height = canvas.height = window.innerHeight;
-    this.spawnC = this.height * 0.1;
-    this.spawnD = this.height * 0.5;
+    this.height = canvas.height = window.innerHeight
+    this.spawnC = this.height * 0.1
+    this.spawnD = this.height * 0.5
   }
 
   onClick(evt) {
-    let x = evt.clientX || (evt.touches && evt.touches[0].pageX);
-    let y = evt.clientY || (evt.touches && evt.touches[0].pageY);
+    let x = evt.clientX || (evt.touches && evt.touches[0].pageX)
+    let y = evt.clientY || (evt.touches && evt.touches[0].pageY)
 
-    let count = random(3, 5);
+    let count = random(3, 5)
     for (let i = 0; i < count; i++)
       this.fireworks.push(
-        new Firework(
-          random(this.spawnA, this.spawnB),
-          this.height,
-          x,
-          y,
-          random(0, 260),
-          random(30, 110)
-        )
-      );
+        new Firework(random(this.spawnA, this.spawnB), this.height, x, y, random(0, 260), random(30, 110)),
+      )
 
-    this.counter = -1;
+    this.counter = -1
   }
 
   update(delta) {
-    ctx.globalCompositeOperation = 'hard-light';
-    ctx.fillStyle = `rgba(20,20,20,${7 * delta})`;
-    ctx.fillRect(0, 0, this.width, this.height);
+    ctx.globalCompositeOperation = 'hard-light'
+    ctx.fillStyle = `rgba(20,20,20,${7 * delta})`
+    ctx.fillRect(0, 0, this.width, this.height)
 
-    ctx.globalCompositeOperation = 'lighter';
-    for (let firework of this.fireworks) firework.update(delta);
+    ctx.globalCompositeOperation = 'lighter'
+    for (let firework of this.fireworks) firework.update(delta)
 
-    this.counter += delta * 3; // Create new firework every second
+    this.counter += delta * 3 // Create new firework every second
     if (this.counter >= 1) {
       this.fireworks.push(
         new Firework(
@@ -69,82 +62,77 @@ class Birthday {
           random(0, this.width),
           random(this.spawnC, this.spawnD),
           random(0, 360),
-          random(30, 110)
-        )
-      );
-      this.counter = 0;
+          random(30, 110),
+        ),
+      )
+      this.counter = 0
     }
 
     // Clean up dead fireworks
-    if (this.fireworks.length > 1000)
-      this.fireworks = this.fireworks.filter(firework => !firework.dead);
+    if (this.fireworks.length > 1000) this.fireworks = this.fireworks.filter(firework => !firework.dead)
   }
 }
 
 class Firework {
   constructor(x, y, targetX, targetY, shade, offsprings) {
-    this.dead = false;
-    this.offsprings = offsprings;
-    this.x = x;
-    this.y = y;
-    this.targetX = targetX;
-    this.targetY = targetY;
-    this.shade = shade;
-    this.history = [];
+    this.dead = false
+    this.offsprings = offsprings
+    this.x = x
+    this.y = y
+    this.targetX = targetX
+    this.targetY = targetY
+    this.shade = shade
+    this.history = []
   }
 
   update(delta) {
-    if (this.dead) return;
+    if (this.dead) return
 
-    let xDiff = this.targetX - this.x;
-    let yDiff = this.targetY - this.y;
+    let xDiff = this.targetX - this.x
+    let yDiff = this.targetY - this.y
 
     if (Math.abs(xDiff) > 3 || Math.abs(yDiff) > 3) {
-      this.x += xDiff * 2 * delta;
-      this.y += yDiff * 2 * delta;
+      this.x += xDiff * 2 * delta
+      this.y += yDiff * 2 * delta
 
-      this.history.push({ x: this.x, y: this.y });
-      if (this.history.length > 20) this.history.shift();
+      this.history.push({ x: this.x, y: this.y })
+      if (this.history.length > 20) this.history.shift()
     } else {
       if (this.offsprings && !this.madeChilds) {
-        let babies = this.offsprings / 2;
+        let babies = this.offsprings / 2
         for (let i = 0; i < babies; i++) {
-          let targetX =
-            this.x + this.offsprings * Math.cos((PI2 * i) / babies) | 0;
-          let targetY =
-            this.y + this.offsprings * Math.sin((PI2 * i) / babies) | 0;
+          let targetX = (this.x + this.offsprings * Math.cos((PI2 * i) / babies)) | 0
+          let targetY = (this.y + this.offsprings * Math.sin((PI2 * i) / babies)) | 0
 
-          birthday.fireworks.push(
-            new Firework(this.x, this.y, targetX, targetY, this.shade, 0)
-          );
+          birthday.fireworks.push(new Firework(this.x, this.y, targetX, targetY, this.shade, 0))
         }
       }
 
-      this.madeChilds = true;
-      this.history.shift();
+      this.madeChilds = true
+      this.history.shift()
     }
 
-    if (this.history.length === 0) this.dead = true;
+    if (this.history.length === 0) this.dead = true
     else if (this.offsprings) {
       for (let i = 0; i < this.history.length; i++) {
-        let point = this.history[i];
-        ctx.beginPath();
-        ctx.fillStyle = `hsl(${this.shade},100%,${i}%)`;
-        ctx.arc(point.x, point.y, 1, 0, PI2, false);
-        ctx.fill();
+        let point = this.history[i]
+        ctx.beginPath()
+        ctx.fillStyle = `hsl(${this.shade},100%,${i}%)`
+        ctx.arc(point.x, point.y, 1, 0, PI2, false)
+        ctx.fill()
       }
     } else {
-      ctx.beginPath();
-      ctx.fillStyle = `hsl(${this.shade},100%,50%)`;
-      ctx.arc(this.x, this.y, 1, 0, PI2, false);
-      ctx.fill();
+      ctx.beginPath()
+      ctx.fillStyle = `hsl(${this.shade},100%,50%)`
+      ctx.arc(this.x, this.y, 1, 0, PI2, false)
+      ctx.fill()
     }
   }
 }
 
 // Confetti Setup
-const particles = [];
-const maxConfettis = 100;
+const particles = []
+const maxConfettis = 50
 const possibleColors = [
   'DodgerBlue',
   'OliveDrab',
@@ -158,62 +146,62 @@ const possibleColors = [
   'SandyBrown',
   'Chocolate',
   'Crimson',
-];
+]
 
 function confettiParticle() {
-  this.x = Math.random() * W;
-  this.y = Math.random() * H - H;
-  this.r = random(11, 33);
-  this.d = Math.random() * maxConfettis + 11;
-  this.color = possibleColors[random(0, possibleColors.length - 1)];
-  this.tilt = random(-10, 10);
-  this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
-  this.tiltAngle = 0;
+  this.x = Math.random() * W
+  this.y = Math.random() * H - H
+  this.r = random(11, 33)
+  this.d = Math.random() * maxConfettis + 11
+  this.color = possibleColors[random(0, possibleColors.length - 1)]
+  this.tilt = random(-10, 10)
+  this.tiltAngleIncremental = Math.random() * 0.07 + 0.05
+  this.tiltAngle = 0
 
   this.draw = function () {
-    ctx.beginPath();
-    ctx.lineWidth = this.r / 2;
-    ctx.strokeStyle = this.color;
-    ctx.moveTo(this.x + this.tilt + this.r / 3, this.y);
-    ctx.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
-    ctx.stroke();
-  };
+    ctx.beginPath()
+    ctx.lineWidth = this.r / 2
+    ctx.strokeStyle = this.color
+    ctx.moveTo(this.x + this.tilt + this.r / 3, this.y)
+    ctx.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5)
+    ctx.stroke()
+  }
 }
 
 // Push new confetti objects to `particles[]`
 for (let i = 0; i < maxConfettis; i++) {
-  particles.push(new confettiParticle());
+  particles.push(new confettiParticle())
 }
 
 // Unified Animation Loop
-const birthday = new Birthday();
-window.onresize = () => birthday.resize();
+const birthday = new Birthday()
+window.onresize = () => birthday.resize()
 
-document.onclick = evt => birthday.onClick(evt);
-document.ontouchstart = evt => birthday.onClick(evt);
+document.onclick = evt => birthday.onClick(evt)
+document.ontouchstart = evt => birthday.onClick(evt)
 
-(function animate() {
-  requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+;(function animate() {
+  requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  const now = timestamp();
-  const delta = (now - birthday.then) / 1000;
-  birthday.then = now;
+  const now = timestamp()
+  const delta = (now - birthday.then) / 1000
+  birthday.then = now
 
   // Update fireworks
-  birthday.update(delta);
+  birthday.update(delta)
 
   // Update confetti
   for (let particle of particles) {
-    particle.draw();
-    particle.tiltAngle += particle.tiltAngleIncremental;
-    particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
-    particle.tilt = Math.sin(particle.tiltAngle) * 15;
+    particle.draw()
+    particle.tiltAngle += particle.tiltAngleIncremental
+    particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2
+    particle.tilt = Math.sin(particle.tiltAngle) * 15
 
     if (particle.y > H) {
-      particle.x = Math.random() * W;
-      particle.y = -30;
-      particle.tilt = random(-20, 20);
+      particle.x = Math.random() * W
+      particle.y = -30
+      particle.tilt = random(-20, 20)
     }
   }
-})();
+})()
